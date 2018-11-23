6d9967f2583141d273f6acdb1e1273591efb8500
Jeroen van den Enden <jvde@netvlies.nl>
2018-11-23 13:07:43 +0000

    Fix strong typing

--- src/Installer.php
+++ src/Installer.php
@@ -110,15 +110,18 @@ final class Installer implements PluginInterface, EventSubscriberInterface
     {
         $changed = false;
 
+        /** @var \RecursiveDirectoryIterator $iterator */
         $iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($sourcePath, \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::SELF_FIRST);
-        foreach ($iterator as $item) {
+
+        /** @var \SplFileInfo $fileInfo */
+        foreach ($iterator as $fileInfo) {
             $target = $targetPath.DIRECTORY_SEPARATOR.$iterator->getSubPathName();
-            if ($item->isDir()) {
+            if ($fileInfo->isDir()) {
                 if (!is_dir($target)) {
                     mkdir($target);
                 }
             } elseif (!file_exists($target)) {
-                $this->copyFile($item, $target);
+                $this->copyFile($fileInfo->getPathname(), $target);
                 $changed = true;
             }
         }
